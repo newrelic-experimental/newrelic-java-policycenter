7aa87fc5fc11ce8b2ef9ad6081bbd5b2d17885bb
Prakash Reddy <preddy@newrelic.com>
2021-05-24 15:48:54 -0400

    first commit

--- README.md
+++ README.md
@@ -1,45 +1,47 @@
-[![New Relic Experimental header](https://github.com/newrelic/opensource-website/raw/master/src/images/categories/Experimental.png)](https://opensource.newrelic.com/oss-category/#new-relic-experimental)
+[![Experimental Project header](https://github.com/newrelic/opensource-website/raw/master/src/images/categories/Experimental.png)](https://opensource.newrelic.com/oss-category/#experimental)
 
-# [Project Name - use format "newrelic-java-<name>"] [build badges go here when available]
+# New Relic Java Instrumentation for Guidewire ClaimCenter and PolicyCenter
 
->[Brief description - what is the project and value does it provide? How often should users expect to get releases? How is versioning set up? Where does this project want to go?]
+Instrumentation for Guidewire applications, including ClaimCenter and PolicyCenter.
 
 ## Installation
 
-> [Include a step-by-step procedure on how to get your code installed. Be sure to include any third-party dependencies that need to be installed separately]
-
-## Getting Started
-
->[Simple steps to start working with the software similar to a "Hello World"]
+1. Obtain the latest release from this repository.
+1. Extract the release into a local directory.
+1. Transfer the extension JAR file to the target server
+1. Copy the extension JAR file into the agent's `extensions` directory (relative to the directory containing the `newrelic.jar` file).
+    *Note:* Create the `extensions` directory if it does not exist.
+1. Restart your JVM
+1. After the app has reloaded, generate traffic against your app that will trigger transactions that you expect to see.
+1. To debug issues, set `log_level` to `finer` in `newrelic.yml`.
 
 ## Usage
 
->[**Optional** - Include more thorough instructions on how to use the software. This section might not be needed if the Getting Started section is enough. Remove this section if it's not needed.]
+Once installed, this extension will perform the following functions:
 
-## Building
+### Transaction Renaming
 
->[**Optional** - Include this section if users will need to follow specific instructions to build the software from source. Be sure to include any third party build dependencies that need to be installed separately. Remove this section if it's not needed.]
+Transactions are named based on the names or values of POST parameters as follows:
 
-## Testing
+* eventSource
+* eventParam - if eventSource is _refresh_ and eventParam is not null
 
->[**Optional** - Include instructions on how to run tests if we include tests with the codebase. Remove this section if it's not needed.]
+### Custom Parameters
+
+These parameters are added to the transactions to which they apply:
+
+* eventSource
+* eventParam
 
 ## Support
 
-New Relic has open-sourced this project. This project is provided AS-IS WITHOUT WARRANTY OR DEDICATED SUPPORT. Issues and contributions should be reported to the project here on GitHub.
-
->[Choose 1 of the 2 options below for Support details, and remove the other one.]
-
->[Option 1 - no specific thread in Community]
->We encourage you to bring your experiences and questions to the [Explorers Hub](https://discuss.newrelic.com) where our community members collaborate on solutions and new ideas.
-
->[Option 2 - thread in Community]
->New Relic hosts and moderates an online forum where customers can interact with New Relic employees as well as other customers to get help and share best practices. Like all official New Relic open source projects, there's a related Community topic in the New Relic Explorers Hub.
->You can find this project's topic/threads here: [URL for Community thread]
+New Relic has open-sourced this project. This project is provided AS-IS WITHOUT WARRANTY OR DEDICATED SUPPORT. Issues and contributions should be reported to the project here on GitHub. We encourage you to bring your experiences and questions to the [Explorers Hub](https://discuss.newrelic.com) where our community members collaborate on solutions and new ideas.
 
 ## Contributing
 
-We encourage your contributions to improve [Project Name]! Keep in mind when you submit your pull request, you'll need to sign the CLA via the click-through using CLA-Assistant. You only have to sign the CLA one time per project. If you have any questions, or to execute our corporate CLA, required if your contribution is on behalf of a company, please drop us an email at opensource@newrelic.com.
+We encourage your contributions to improve newrelic-java-guidewire! Keep in mind when you submit your pull request, you'll need to sign the CLA via the click-through using CLA-Assistant. You only have to sign the CLA one time per project.
+
+If you have any questions, or to execute our corporate CLA, required if your contribution is on behalf of a company,  please drop us an email at opensource@newrelic.com.
 
 **A note about vulnerabilities**
 
@@ -48,7 +50,5 @@ As noted in our [security policy](../../security/policy), New Relic is committed
 If you believe you have found a security vulnerability in this project or any of New Relic's products or websites, we welcome and greatly appreciate you reporting it to New Relic through [HackerOne](https://hackerone.com/newrelic).
 
 ## License
-
-[Project Name] is licensed under the [Apache 2.0](http://apache.org/licenses/LICENSE-2.0.txt) License.
-
->[If applicable: [Project Name] also uses source code from third-party libraries. You can find full details on which libraries are used and the terms under which they are licensed in the third-party notices document.]
+newrelic-java-guidewire is licensed under the [Apache 2.0](http://apache.org/licenses/LICENSE-2.0.txt) License.
+>[If applicable: The newrelic-java-guidewire also uses source code from third-party libraries. You can find full details on which libraries are used and the terms under which they are licensed in the third-party notices document.]
---
+++ src/main/java/com/newrelic/guidewire/HttpServlet_instrumentation.java
@@ -0,0 +1,169 @@
+package com.newrelic.guidewire;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.newrelic.api.agent.NewRelic;
+import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.TransactionNamePriority;
+import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.NewField;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.Weaver;
+
+@Weave(originalName = "javax.servlet.http.HttpServlet", type = MatchType.BaseClass)
+public abstract class HttpServlet_instrumentation {
+
+	@NewField
+	private static Map<String, List<String>> selectedJsonParametersMap = null;
+	
+	@NewField
+	private final static String PARAM_EVENT_SOURCE = "eventSource";
+	
+	@NewField
+	private final static String PARAM_EVENT_PARAM = "eventParam";
+	
+	@NewField
+	private final static String ROOT_ATTRIBUTES_KEY = "rootAttributes";
+
+	@NewField
+	private static JsonFactory jsonFactory = new JsonFactory();
+
+	public HttpServlet_instrumentation() {
+		initJsonParametersMap();
+	}
+
+	@Trace(dispatcher=true)
+	protected void service(HttpServletRequest request, HttpServletResponse response) {
+		Weaver.callOriginal();
+		try {
+			String wizardName = null;
+			if (request != null) {
+				String eventSource = request.getParameter(PARAM_EVENT_SOURCE);
+				if ((eventSource != null)) {
+					NewRelic.addCustomParameter(PARAM_EVENT_SOURCE, eventSource);
+					String eventParam = request.getParameter(PARAM_EVENT_PARAM);
+					if(eventParam != null && !eventParam.isEmpty())
+					{
+						//NewRelic.getAgent().getTransaction().setTransactionName(TransactionNamePriority.CUSTOM_HIGH, true, PARAM_EVENT_PARAM, eventParam);
+						NewRelic.setTransactionName("Custom", eventParam);
+						NewRelic.addCustomParameter(PARAM_EVENT_PARAM, eventParam);
+					}
+					else
+					{
+						//NewRelic.getAgent().getTransaction().setTransactionName(TransactionNamePriority.CUSTOM_HIGH, true, PARAM_EVENT_SOURCE, eventSource);
+						NewRelic.setTransactionName("Custom", eventSource);
+					}
+					
+					if (eventSource.endsWith("_act")) {
+						int lengthWizardName = eventSource.indexOf("-");
+						if (lengthWizardName > 1) {
+							wizardName = eventSource.substring(0, lengthWizardName);
+						}
+					}
+				}
+
+				Map<String, String[]> parametersMap = request.getParameterMap();
+				for (String paramName : parametersMap.keySet()) {
+					/*
+					String paramDisplayName = selectedParametersMap.get(paramName);
+
+					if (paramDisplayName != null) {
+						String[] pValue = request.getParameterValues(paramName);
+						if ((pValue != null) && (pValue.length > 0)) {
+							String pValueString = String.join(" ", pValue);
+							if (!pValueString.isEmpty()) {
+								NewRelic.addCustomParameter(paramDisplayName, pValueString);
+							}
+						}
+					}
+					*/
+
+					if ((wizardName != null) && (paramName.startsWith(wizardName))) {
+						String jsonContent = request.getParameter(paramName);
+						if ((jsonContent != null) && (!jsonContent.isEmpty())) {
+							try (JsonParser jsonParser = jsonFactory.createParser(jsonContent)) {
+								while (jsonParser.nextToken() != null) {
+									JsonToken token = jsonParser.getCurrentToken();
+									if (token.equals(JsonToken.FIELD_NAME)) {
+										String fieldName = jsonParser.getCurrentName();
+										jsonParser.nextToken();
+										if (jsonParser.getCurrentToken().equals(JsonToken.START_OBJECT)) {
+											List<String> listAttributes = selectedJsonParametersMap.get(fieldName);
+											int depth = 0;
+											while ((jsonParser.nextToken() != JsonToken.END_OBJECT) || (depth > 0)) {
+												JsonToken nestedToken = jsonParser.currentToken();
+												if (nestedToken.equals(JsonToken.START_OBJECT)) {
+													depth++;
+												}
+												if (nestedToken.equals(JsonToken.END_OBJECT)) {
+													depth--;
+												}
+
+												if (listAttributes != null) {
+													if (nestedToken.equals(JsonToken.FIELD_NAME)) {
+														String name = jsonParser.getCurrentName();
+														jsonParser.nextToken();
+														String value = jsonParser.getValueAsString();
+														if (listAttributes.contains(name)) {
+															NewRelic.addCustomParameter(name, value);
+														}
+													}
+												}
+											}
+										}
+										List<String> rootAttributes = selectedJsonParametersMap.get(ROOT_ATTRIBUTES_KEY);
+										if (rootAttributes.contains(fieldName)) {
+											String fieldValue = jsonParser.getValueAsString();
+											NewRelic.addCustomParameter(fieldName, fieldValue);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			NewRelic.getAgent().getLogger().log(Level.SEVERE,
+					"Exception in NewRelic Guidewire extension " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	private void initJsonParametersMap() {
+		if (selectedJsonParametersMap != null) {
+			// must have initialized in another thread
+			NewRelic.getAgent().getLogger().log(Level.INFO,
+					"GUIDEWIRE extension JSON parameter map already initialized");
+			return;
+		}
+
+		selectedJsonParametersMap = new HashMap<String, List<String>>();
+
+		List<String> rootAttributes = new ArrayList<String>();
+		rootAttributes.add("jobNumber");
+		rootAttributes.add("currentLocation");
+		rootAttributes.add("serviceName");
+		rootAttributes.add("newBuildingNumber");
+		rootAttributes.add("moduleNumber");
+		selectedJsonParametersMap.put(ROOT_ATTRIBUTES_KEY, rootAttributes);
+
+		List<String> piAttributes = new ArrayList<String>();
+		piAttributes.add("productCode");
+		piAttributes.add("jobType");
+		piAttributes.add("quoteType");
+		piAttributes.add("policyType");
+		selectedJsonParametersMap.put("productInformation", piAttributes);
+	}
+
+}
